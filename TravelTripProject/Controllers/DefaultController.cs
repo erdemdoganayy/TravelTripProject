55120a12c6253946d3e16ef0413c7f2956389d09
Erdem Doğanay <erdemdoganayiletisim@gmail.com>
2022-06-27 22:57:27 +0300

    Sayfalar Düzenlendi

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -13,5 +13,15 @@ namespace TravelTripProject.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
     }
 }
